b64a751d33d5a581f66f9f3d5279cabbd68bf3c0
Sajid Jahabar Ali <sajidjahabarali@gmail.com>
2023-01-30 12:24:03 +0000

    completed lesson 5

---
+++ 05-transform-objects/src/script.js
@@ -0,0 +1,74 @@
+import * as THREE from "three";
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Objects
+ */
+const group = new THREE.Group();
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+
+cube2.position.x = 2;
+cube3.position.y = 2;
+
+group.add(cube1, cube2, cube3);
+
+group.rotateX(1);
+// group.rotateY(1);
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh = new THREE.Mesh(geometry, material);
+// mesh.scale.set(3, 1, 1);
+// mesh.rotation.reorder("YXZ");
+// mesh.rotation.y = 2.7;
+// mesh.rotation.x = 1.6;
+// mesh.rotation.z = 1.5;
+// scene.add(mesh);
+
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+camera.position.y = 1;
+scene.add(camera);
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
